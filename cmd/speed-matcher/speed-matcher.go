f2b9d4a3cd8c5e3ea47d10fec841adedfd12f4df
CurlyQuokka <pstrusiewiczsurmacki@gmail.com>
2024-02-05 01:39:36 +0100

    Moved server-related functions to separate package
    Signed-off-by: CurlyQuokka <pstrusiewiczsurmacki@gmail.com>

--- cmd/speed-matcher/speed-matcher.go
+++ cmd/speed-matcher/speed-matcher.go
@@ -2,21 +2,13 @@ package main
 
 import (
 	"crypto/cipher"
-	"encoding/json"
-	"fmt"
-	"html/template"
 	"log"
-	"net/http"
-	"net/smtp"
 	"os"
-	"regexp"
 	"strconv"
 	"strings"
 
-	"github.com/CurlyQuokka/speed-matcher/pkg/csvreader"
-	"github.com/CurlyQuokka/speed-matcher/pkg/participant"
-	. "github.com/CurlyQuokka/speed-matcher/pkg/result"
 	"github.com/CurlyQuokka/speed-matcher/pkg/security"
+	"github.com/CurlyQuokka/speed-matcher/pkg/server"
 )
 
 const (
@@ -40,177 +32,6 @@ var (
 	defaultSecretLength = 16
 	domains             []string
 )
-
-func uploadHandler(w http.ResponseWriter, r *http.Request) {
-	var err error
-
-	if r.Method != "POST" {
-		http.Error(w, "method not allowed", http.StatusMethodNotAllowed)
-		return
-	}
-
-	r.Body = http.MaxBytesReader(w, r.Body, maxUploadSize)
-	if err = r.ParseMultipartForm(maxUploadSize); err != nil {
-		http.Error(w, "the uploaded file is too big. Please choose an file that's less than 1MB in size", http.StatusBadRequest)
-		return
-	}
-
-	fromEmail := r.FormValue("fromEmail")
-
-	if len(domains) > 0 {
-		validEmail := false
-		for _, domain := range domains {
-			domainRegex := "^[\\w-\\.]+@" + strings.ReplaceAll(domain, ".", "\\.")
-			fmt.Println(domainRegex)
-			r, err := regexp.Compile(domainRegex)
-			if err != nil {
-				http.Error(w, err.Error(), http.StatusInternalServerError)
-				return
-			}
-			if r.MatchString(fromEmail) {
-				validEmail = true
-				break
-			}
-		}
-		if !validEmail {
-			http.Error(w, "email "+fromEmail+" is not allowed", http.StatusBadRequest)
-			return
-		}
-	}
-
-	passwordEmail := strings.ReplaceAll(r.FormValue("passwordEmail"), " ", "")
-
-	pass, err := sec.Encrypt(passwordEmail)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-
-	participantsFile, _, err := r.FormFile("participants")
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusBadRequest)
-		return
-	}
-
-	defer participantsFile.Close()
-
-	reader := csvreader.CSVReader{}
-
-	err = reader.LoadDataFromFile(participantsFile, "participants")
-	if err != nil {
-		http.Error(w, "failed to load participants data", http.StatusInternalServerError)
-		return
-	}
-
-	participants, err := participant.ConvertCSVData(reader.Data)
-	if err != nil {
-		http.Error(w, "failed to process participants data: "+err.Error(), http.StatusBadRequest)
-		return
-	}
-
-	matchesFile, _, err := r.FormFile("matches")
-	if err != nil {
-		http.Error(w, "filed to load matching data:"+err.Error(), http.StatusInternalServerError)
-		return
-	}
-
-	defer participantsFile.Close()
-
-	err = reader.LoadDataFromFile(matchesFile, "matches")
-	if err != nil {
-		http.Error(w, "failed to load matching data: "+err.Error(), http.StatusInternalServerError)
-		return
-	}
-
-	err = participants.LoadMatches(reader.Data)
-	if err != nil {
-		http.Error(w, "failed to process matching data: "+err.Error(), http.StatusInternalServerError)
-		return
-	}
-
-	participants.ProcessMatches()
-
-	otp, err := sec.GenerateOTP()
-	if err != nil {
-		http.Error(w, "failed to generate OTP: "+err.Error(), http.StatusInternalServerError)
-		return
-	}
-
-	matchingResults := Result{
-		FromEmail:     fromEmail,
-		PasswordEmail: pass,
-		EventName:     r.FormValue("eventName"),
-		Participants:  participants,
-		OTP:           otp,
-	}
-
-	tmpl, err := template.New("result.gohtml").Funcs(template.FuncMap{
-		"Deref": func(p *participant.Participant) participant.Participant {
-			return *p
-		},
-	}).ParseFiles("templates/result.gohtml")
-	if err != nil {
-		http.Error(w, "failed to prepare matching template: "+err.Error(), http.StatusInternalServerError)
-		return
-	}
-
-	err = tmpl.Execute(w, matchingResults)
-	if err != nil {
-		http.Error(w, "failed to execute result template: "+err.Error(), http.StatusInternalServerError)
-		return
-	}
-}
-
-type ToSend struct {
-	From    string `json:"from"`
-	Pass    string `json:"pass"`
-	To      string `json:"to"`
-	Subject string `json:"subject"`
-	Content string `json:"content"`
-	Otp     string `json:"otp"`
-}
-
-func mailHandler(w http.ResponseWriter, r *http.Request) {
-	if r.Method != "POST" {
-		http.Error(w, "method not allowed", http.StatusMethodNotAllowed)
-		return
-	}
-
-	var ts ToSend
-	err := json.NewDecoder(r.Body).Decode(&ts)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusBadRequest)
-		return
-	}
-
-	if !sec.CheckOTP(ts.Otp) {
-		http.Error(w, "OTP expired", http.StatusBadRequest)
-		return
-	}
-
-	mime := "MIME-version: 1.0;\nContent-Type: text/html; charset=\"UTF-8\";\n\n"
-
-	msg := "From: Stowarzyszenie Lambda Szczecin\n" +
-		"To: " + ts.To + "\n" +
-		"Subject: " + ts.Subject + "\n" +
-		mime + ts.Content
-
-	smtpGmail := "smtp.gmail.com"
-	server := smtpGmail + ":587"
-
-	pass, err := sec.Decrypt(ts.Pass)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-
-	auth := smtp.PlainAuth("Stowarzyszenie Lambda Szczecin", ts.From, pass, smtpGmail)
-
-	if err := smtp.SendMail(server, auth, ts.From, []string{ts.To}, []byte(msg)); err != nil {
-		http.Error(w, "Error while sending to: "+ts.To+" "+err.Error(), http.StatusInternalServerError)
-		return
-	}
-}
 
 func main() {
 	var err error
@@ -258,14 +79,9 @@ func main() {
 		domains = strings.Split(domainString, ",")
 	}
 
-	http.Handle("/", http.FileServer(http.Dir("frontend/")))
+	s := server.New(maxUploadSize, domains, sec)
 
-	http.Handle("/scripts/", http.StripPrefix("/scripts/", http.FileServer(http.Dir("scripts"))))
-
-	http.HandleFunc("/result", uploadHandler)
-	http.HandleFunc("/mail", mailHandler)
-
-	err = http.ListenAndServe(":"+port, nil)
+	err = s.Serve(port)
 	if err != nil {
 		log.Fatal(err)
 	}
